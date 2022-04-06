cec5f62322dd2d3cc5c31846c2a4739460cb5609
SOM-0x3B1 <somadagaszkar@gmail.com>
2022-04-06 21:34:28 +0200

    Add static style

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,3 @@
-document.documentElement.style.setProperty("--main-background-color", "rgb(22, 22, 22)");
+document.documentElement.style.setProperty("--main-background-color", "rgb(18, 20, 15)");
 document.documentElement.style.setProperty("--secondary-background-color", "rgb(12, 12, 12)");
-document.documentElement.style.setProperty("--main-foreground-color", "rgb(149, 255, 190)");
+document.documentElement.style.setProperty("--main-foreground-color", "rgb(149, 255, 140)");
