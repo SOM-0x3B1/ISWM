62ed13e720c8cac7ab7b50124bd1187ab5416223
SOM-0x3B1 <somadagaszkar@gmail.com>
2022-05-02 18:41:23 +0200

    Add end of countdown message

--- js/index.js
+++ js/index.js
@@ -8,7 +8,7 @@ const pMinute = pSecond * 60;
 const pHour = pMinute * 60;
 const pDay = pHour * 24;
 
-const countDownDate = new Date("2022-05-02T21:00:00.000+02:00").getTime();
+const countDownDate = new Date("2022-05-02T18:00:00.000+02:00").getTime();
 const originDate = new Date("2022-04-04T21:00:00.000+02:00").getTime();
 
 const h2_1 = "REBOOTING UNIVERSE";
@@ -72,33 +72,43 @@ function startCountDown() {
         // Find the distance between now and the count down date
         let distance = countDownDate - now;
 
-        // Time calculations for days, hours, minutes and seconds
-        let days = Math.floor(distance / pDay);
-        let hours = Math.floor((distance % (pDay)) / (pHour));
-        let minutes = Math.floor((distance % (pHour)) / (pMinute));
-        let seconds = Math.floor((distance % (pMinute)) / pSecond);
-
-        // Display the result in the element with id="demo"
-        document.getElementById("time").innerText = days + " DAYS " + hours + " HOURS "
-            + minutes + " MINUTES " + seconds + " SECONDS ";
-
-        let percentage = (Math.round((100 - (countDownDate - now) / (countDownDate - originDate) * 100) * 100) / 100) + '%'
-
-
-        if (percentage != lastPercentage) {
-            aupdate.play();
-            lastPercentage = percentage;
-            let perc1 = document.getElementById("percentage");
-
-            perc1.innerText = percentage;
-
-            document.getElementById("fill").style.width = percentage;
-        }
-
-        // If the count down is finished, write some text
         if (distance < 0) {
             clearInterval(x);
-            document.getElementById("time").innerHTML = "REBOOTED";
+            document.getElementById('h2').innerHTML = 'UNIVERSE REBOOTED<br>THANK YOU FOR WATCHING';
+            document.getElementById('cursor').innerText = '♥';      
+            document.getElementById("cursor").style.fontSize = '20pt';     
+
+            document.getElementById("percentage").innerText = '100%';            
+            document.getElementById("fill").style.width = '100%';
+            document.getElementById("p_1").innerText = "THE WEBSITE WILL BE REPURPOSED,";
+            document.getElementById("time").innerText = "so don't forget about this place".toUpperCase();
+        }
+        else {
+
+            // Time calculations for days, hours, minutes and seconds
+            let days = Math.floor(distance / pDay);
+            let hours = Math.floor((distance % (pDay)) / (pHour));
+            let minutes = Math.floor((distance % (pHour)) / (pMinute));
+            let seconds = Math.floor((distance % (pMinute)) / pSecond);
+
+            // Display the result in the element with id="demo"
+            document.getElementById("time").innerText = days + " DAYS " + hours + " HOURS "
+                + minutes + " MINUTES " + seconds + " SECONDS ";
+
+            let percentage = (Math.round((100 - (countDownDate - now) / (countDownDate - originDate) * 100) * 100) / 100) + '%'
+
+
+            if (percentage != lastPercentage) {
+                aupdate.play();
+                lastPercentage = percentage;
+                let perc1 = document.getElementById("percentage");
+
+                perc1.innerText = percentage;
+
+                document.getElementById("fill").style.width = percentage;
+            }
+
+            // If the count down is finished, write some text
         }
     }, 1000);
 }
